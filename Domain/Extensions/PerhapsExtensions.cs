b35904ae0835e61dc3fd393dd296f1fa24ae16d1
Noah <noahgbuss@gmail.com>
2024-12-04 17:32:12 -0600

    Added FirstOrPerhaps and LastOrPerhaps with func parameters

--- Domain/Extensions/PerhapsExtensions.cs
+++ Domain/Extensions/PerhapsExtensions.cs
@@ -3,6 +3,53 @@
 namespace Vulpes.Electrum.Domain.Extensions;
 public static class PerhapsExtensions
 {
-    public static Perhaps<TItem> FirstToPerhaps<TItem>(this IEnumerable<TItem> items) => items.Any() ? Perhaps<TItem>.ToPerhaps(items.First()) : Perhaps<TItem>.Empty;
-    public static Perhaps<TItem> LastToPerhaps<TItem>(this IEnumerable<TItem> items) => items.Any() ? Perhaps<TItem>.ToPerhaps(items.Last()) : Perhaps<TItem>.Empty;
+    public static Perhaps<TItem> FirstOrPerhaps<TItem>(this IEnumerable<TItem> items)
+    {
+        using (var enumerator = items.GetEnumerator())
+        {
+            if (enumerator.MoveNext())
+            {
+                return Perhaps<TItem>.ToPerhaps(enumerator.Current);
+            }
+        }
+
+        return Perhaps<TItem>.Empty;
+    }
+
+    public static Perhaps<TItem> FirstOrPerhaps<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> predicate)
+    {
+        using (var enumerator = items.Where(predicate).GetEnumerator())
+        {
+            if (enumerator.MoveNext())
+            {
+                return Perhaps<TItem>.ToPerhaps(enumerator.Current);
+            }
+        }
+
+        return Perhaps<TItem>.Empty;
+    }
+
+    public static Perhaps<TItem> LastToPerhaps<TItem>(this IEnumerable<TItem> items)
+    {
+        var choice = Perhaps<TItem>.Empty;
+
+        foreach (var item in items)
+        {
+            choice = Perhaps<TItem>.ToPerhaps(item);
+        }
+
+        return choice;
+    }
+
+    public static Perhaps<TItem> LastToPerhaps<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> predicate)
+    {
+        var choice = Perhaps<TItem>.Empty;
+
+        foreach (var item in items.Where(predicate))
+        {
+            choice = Perhaps<TItem>.ToPerhaps(item);
+        }
+
+        return choice;
+    }
 }
